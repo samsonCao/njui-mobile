d10b6fbd7f8cbc1e18b37a7207a046c6e4d1caae
rmlzy <liuzhiyuan2018@gmail.com>
2018-10-05 20:39:34 +0800

    [Dev] Add Blank Checkbox

---
+++ packages/Checkbox/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {classNames} from '../utils';
+import './index.scss'
+
+class Checkbox extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+Checkbox.propTypes = {};
+Checkbox.defaultProps = {};
+
+export default Checkbox;
